8d5c8cdbada5738d8c1e988a3dbd407118aa733c
Nester <alechebil@mail.ru>
2025-04-14 19:47:23 +0300

    3. Состояние нити.

---
+++ src/main/java/ru/tasks/ThreadState.java
@@ -0,0 +1,28 @@
+package ru.tasks;
+
+public class ThreadState {
+
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {
+                }
+        );
+        Thread second = new Thread(
+                () -> {
+                }
+        );
+        first.start();
+        second.start();
+        System.out.println(first.getName());
+        System.out.println(second.getName());
+
+        while (first.getState() != Thread.State.TERMINATED
+                && second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getState());
+            System.out.println(second.getState());
+        }
+
+        System.out.println("first thread - " + first.getState());
+        System.out.println("second thread - " + second.getState());
+    }
+}
