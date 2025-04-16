9faa3bca1b0f7aeca98817e7d0e7708c8d771750
Nester <alechebil@mail.ru>
2025-04-16 19:16:41 +0300

    2. Модель памяти Java

---
+++ src/main/java/ru/tasks/DCLSingleton.java
@@ -0,0 +1,21 @@
+package main.java.ru.tasks;
+
+public final class DCLSingleton {
+
+    private static volatile DCLSingleton instance;
+
+    private DCLSingleton() {
+    }
+
+    public static DCLSingleton getInstance() {
+        if (instance == null) {
+            synchronized (DCLSingleton.class) {
+                if (instance == null) {
+                    instance = new DCLSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+}
